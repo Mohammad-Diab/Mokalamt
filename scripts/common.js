20f0222ff1417b3d314832e4dea0770b2966669e
Mohammad Diab <mohammad.Diab93@outlook.com>
2023-01-02 11:48:27 +0400

    fix urls

--- scripts/common.js
+++ scripts/common.js
@@ -1,6 +1,6 @@
 
-var ServerUrl = "https://localhost:7098/Mokalamat/";
-var ClientUrl = "http://localhost/HwSite";
+var ServerUrl = "http://mohammaddiab1-001-site1.atempurl.com/Mokalamat/";
+var ClientUrl = "https://mohammad-diab.github.io/Mokalamt";
 
 function requestToServer(url, type, data, onSuccess) {
     data = type.toLowerCase() == 'get' ? '' : JSON.stringify(data);
